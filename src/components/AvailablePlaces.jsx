aa1ac64377a350aba83b8caaa00d36432406c63d
@NelProgrammer <nelsonlzweni@gmail.com>
2024-07-10 18:51:00 +0200

    Added Error Handling. E.g. Incorrect Server address punched, no network etc.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,29 +1,50 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
-// import { response } from 'express';
+import Error from './Error.jsx';
 
 const AvailablePlaces = ({ onSelectPlace }) => {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     const fetchPlaces = async () => {
       setIsFetching(true);
-      // const response = await fetch('/places');
-      const response = await fetch('http://localhost:3001/places');
-      console.log(`**** Response Fetched.`);
 
-      const resData = await response.json();
-      console.log(`**** ResponseData Fetched.`);
+      try {
+        const response = await fetch('http://localhost:3001/places');
+        console.log(`**** Response Fetched.`);
 
-      setAvailablePlaces(resData.places);
+        const resData = await response.json();
+        console.log(`**** ResponseData Fetched.`);
+
+        if (!response.ok) {
+          console.log(`**** Throw Error if response is not OK.`);
+          throw new Error('Failed to fetch places.');
+        }
+
+        console.log(`**** Set AvailablePlaces.`);
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        // Code if Error
+        setError({
+          message: error.message || 'Could not fetch places, try again latter.',
+        });
+        console.log(`**** Set Error Message.`);
+      }
+
       console.log(`**** Available Places.`);
 
-      setIsFetching(false);
+      setIsFetching(false); // Outside to ensure we still change fetching state even if there's an error.
     };
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    console.log(`**** Error returned.`);
+    return <Error title="An error occured!" message={error.message} />;
+  }
 
   return (
     <Places
